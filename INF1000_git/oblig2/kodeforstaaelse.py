a54171cae2547b3d2e5b18697399cfe45bbc2235
Toanbego <t.a.b.gombos@fys.uio.no>
2018-07-31 17:49:41 +0200

    sure

--- INF1000_git/oblig2/kodeforstaaelse.py
+++ INF1000_git/oblig2/kodeforstaaelse.py
@@ -1,8 +1,8 @@
 
 """Add a question and answer in the string. seperate the two with a : """
 questions = """Best album of all time? :Back in black
-                Earths gravitation? :9.81 
-                Who plays Aragorn in lord of the rings? :Viggo Mortensen 
+                Earths gravitation? :9.81
+                Who plays Aragorn in lord of the rings? :Viggo Mortensen
                 What is love? :Baby dont hurt me"""
 
 listOfQuestions = []
@@ -10,7 +10,7 @@ listOfAnswers = []
 
 #Split string into question and answer
 split_string = questions.split('\n')
-print(split_string)
+
 for i in split_string:
     question, answer = i.split(':')
     listOfQuestions.append(question)
