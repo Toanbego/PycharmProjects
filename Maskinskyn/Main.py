779b71ec49397b24f8ad7b506dc1be40fa979892
LAPTOP-AFNS1ELH\toanbego <torstein.gombos@gmail.com>
2019-01-30 13:38:27 +0100

    no message

--- Maskinskyn/Main.py
+++ Maskinskyn/Main.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Thu Apr 26 09:51:31 2018
 
@@ -14,7 +13,7 @@ import csv
 import re
 import copy
 
-"""Globale variabler"""
+# Globale variabler
 
 # Data som kan bli brukt/brukes, for tracking og data til klassifikator
 storeHeight = []
@@ -30,7 +29,7 @@ treerkast = 0
 firerkast = 0
 enerkast = 0
 Score = 0
-Class = 0
+classification = 0
 
 checkYdir = (deque(maxlen=2),
              deque(maxlen=2),
@@ -50,14 +49,17 @@ checkXdir = (deque(maxlen=2),
              deque(maxlen=2),
              deque(maxlen=2),
              deque(maxlen=2),)
-VideoFile = 'test/3test.mov'
+
+# Load video
+VideoFile = 'test/4test.mov'
 # VideoFile = 'Data/video/show2.mov'
 
+# Load training set
 text_file = np.loadtxt('Trening1750samples.txt', dtype=np.float32)
-
 k = np.arange(2)
 train_labels = np.repeat(k, 880)[:, np.newaxis].astype(np.float32)
 
+# Train model
 knn = cv2.ml.KNearest_create()
 knn.train(text_file, cv2.ml.ROW_SAMPLE, train_labels)
 
@@ -72,9 +74,6 @@ def stream():
     # Dette er HSV terskelene for å filtere ut bakgrunn.
     # Vi har brukt programmet rangeDetector.py til å manuelt finne gode verdier
 
-    #    Red
-    #    colorLower = (0, 138, 159)
-    #    colorUpper = (213, 255, 255)
     # red
     colorLower = (166, 180, 86)
     colorUpper = (213, 255, 255)
@@ -86,14 +85,12 @@ def stream():
     #    #Green
     #    colorLower = (27, 110, 68)
     #    colorUpper = (95, 255, 255)
-    #
-    # Green - Live kontor
-    #    colorLower = (134, 176, 16)
-    #    colorUpper = (199, 255, 255)
+
 
     # =============================================================================
     #     Initialisering av globale variabler
     # =============================================================================
+
     # ptsTot er ett sett med deque tuples som vil lagre hvert koordinat til objektene våre.
     # Max 7 objekter av ganger foreløpig
     mlen = 45
@@ -114,7 +111,7 @@ def stream():
             ptsTot[ii].appendleft((0, 0))
 
     # =============================================================================
-    #             While-løkka starter
+    #             While-loop starter
     # =============================================================================
 
     while (1):
@@ -124,18 +121,9 @@ def stream():
 
         # Leser en frame fra kameraet/videoen
         _, frame = cap.read()
-        #
 
-        # Her blures først bildet før det blir konvert til till HSV verdier
-        blur = cv2.GaussianBlur(frame, (7, 7), 0)
-        hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-
-        # Lager en maske for bildet innenfor HSV grensene vi satte i starten av programmet.
-        # Deretter eroder vi bildet for å fjære små korn som er igjen.
-        # Etterpå dilates det slik at konturene av objektet blir litt kraftigere
-        mask = cv2.inRange(hsv, colorLower, colorUpper)
-        mask = cv2.erode(mask, None, 2)
-        mask = cv2.dilate(mask, None, 5)
+        # Perform segmentation
+        mask = morphological_operations(frame, colorLower, colorUpper)
 
         # Finner konturene i masken vår
         cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
@@ -144,13 +132,10 @@ def stream():
         if len(cnts) > 0:
 
             # Finner den største konturen og definerer ett threshold som er 40% av den største konturen
-            # Dette brukes til æ filtrere bort små blobs og kune fokusere på de store konturene
             c = max(cnts, key=cv2.contourArea)
-            # tåler 40% minimum størrelse på konturen
-            objectSizeThreshold = np.size(c) * 0.4
 
-            # Initialiserer våre kommende center for ballene
-            center = None
+            # Tåler 40% minimum størrelse på konturen
+            objectSizeThreshold = np.size(c) * 0.4
 
             # Beregner og skriver inn antall objekter på skjermen.
             numOfObjects = len([1 for ii in cnts if np.size(ii) > objectSizeThreshold])
@@ -160,15 +145,14 @@ def stream():
             # Går gjennom hver kontur og finner senterpunktet vedhjelp av cv2.moments
             for element in cnts:
                 if np.size(element) > objectSizeThreshold:
-                    # Tegner sirkel rundt detektert objekt
+
+                    # Find object coordinates
                     ((x, y), radius) = cv2.minEnclosingCircle(element)
                     M = cv2.moments(element)
                     # Beregner center koordinat
                     center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-                    # Hvis radius er stor er stor nok tegner vi sirkel rundt objektene våre.
-                    # En liten i midten, og en større rundt objektet
-
+                    # Draw circle
                     if radius > 5:
                         cv2.circle(frame, center, 5, (0, 255, 255), -1)
                         cv2.circle(frame, (int(x), int(y)), int(radius) + 2, (0, 255, 255), 2)
@@ -176,38 +160,12 @@ def stream():
                     # Hvert senter i framen blir lagret i pts
                     pts.append(center)
 
-                    collectData(ptsTot, counter, frame, numOfObjects, radius, checkYdir, throwCounter)
-            # Tar for seg hvert kordinat "i" en nye frame.
-            # Deretter sammenlikner den koordinatet med hvert av de gamle koordinatene i forrige frame
-            # Den vil så finne ut hvilken av de gamle som var nærmest og sørge for at kooridnatene
-            # blir lagret i samme deque
-            k = 0
-            for i in pts:
+                    collectData(ptsTot, counter, frame, numOfObjects, radius, checkYdir)
 
-                # Initaliserer distansen mellom hvert senter
-                dist = []
-                # Looper gjennom hver av koordinatene i den gamle framen
-                for j in range(0, len(ptsTot)):
-                    # Finner distansen mellom det nye koordinatet vårt og til de 3 gamle.
-                    # Så finner vi indeksen hvor dette koordinatet ligger
-                    dist.append(np.linalg.norm
-                                (i[0] - ptsTot[j][0][0]) + np.linalg.norm(i[1] - ptsTot[j][0][1]))
-                minDistIdx = np.argmin(dist)
+            # Track the detected objects
+            track_objects(pts, ptsTot, frame)
 
-                # If settning som skal filtrere bort avstander som er for store.
-                # dvs. feil klassifisering
-                if dist[minDistIdx] < 150:
-                    # Tar det gjeldende koordinatet vi tester for å plasserer det i riktig deque
-                    ptsTot[minDistIdx].appendleft(i)
-
-                    # Hvis sdet ikke har er noe objekt så ikke tegn noe
-                    for j in range(1, len(ptsTot[minDistIdx])):
-                        if ptsTot[minDistIdx][j - 1] == None or ptsTot[minDistIdx][j] == None:
-                            continue
-                        thickness = int(np.sqrt(25 / float(j + 1)) * 2)
-                        cv2.line(frame, ptsTot[minDistIdx][j], ptsTot[minDistIdx][j - 1], (0, 0, 255), thickness)
-
-            # Lagrer de første koordinatene våre i ptsTot
+            # Saves the first coordinates in ptsTot
             if ptsTot[0][0] == (0, 0):
                 for ii in range(0, numOfObjects):
                     ptsTot[ii].appendleft(pts[ii])
@@ -235,18 +193,74 @@ def stream():
     cv2.waitKey(1)
 
 
-# =============================================================================
-# Funksjonene våre
-# =============================================================================
+# def detect_objects():
 
 
-# =============================================================================
-# #Denne blir implementert i funksjonen drawTail()
-# =============================================================================
+
+def track_objects(pts,ptsTot,frame):
+    """
+    Tar for seg hvert kordinat "i" en nye frame.
+    Deretter sammenlikner den koordinatet med hvert av de gamle koordinatene i forrige frame
+    Den vil så finne ut hvilken av de gamle som var nærmest og sørge for at kooridnatene
+    blir lagret i samme deque
+    :param pts:
+    :param ptsTot:
+    :param frame:
+    :return:
+    """
+
+    for i in pts:
+        # Initaliserer distansen mellom hvert senter
+        dist = []
+        # Looper gjennom hver av koordinatene i den gamle framen
+        for j in range(0, len(ptsTot)):
+            # Finner distansen mellom det nye koordinatet vårt og til de 3 gamle.
+            # Så finner vi indeksen hvor dette koordinatet ligger
+            dist.append(np.linalg.norm
+                        (i[0] - ptsTot[j][0][0]) + np.linalg.norm(i[1] - ptsTot[j][0][1]))
+        minDistIdx = np.argmin(dist)
+
+        # If settning som skal filtrere bort avstander som er for store.
+        # dvs. feil klassifisering
+        if dist[minDistIdx] < 150:
+            # Tar det gjeldende koordinatet vi tester for å plasserer det i riktig deque
+            ptsTot[minDistIdx].appendleft(i)
+
+            # Hvis sdet ikke har er noe objekt så ikke tegn noe
+            for j in range(1, len(ptsTot[minDistIdx])):
+                if ptsTot[minDistIdx][j - 1] == None or ptsTot[minDistIdx][j] == None:
+                    continue
+                thickness = int(np.sqrt(25 / float(j + 1)) * 2)
+                cv2.line(frame, ptsTot[minDistIdx][j], ptsTot[minDistIdx][j - 1], (0, 0, 255), thickness)
+
+
+def morphological_operations(frame, colorLower, colorUpper, erode=2, dilate=5):
+    """
+    Lager en maske for bildet innenfor HSV grensene vi satte i starten av programmet.
+    Deretter eroder vi bildet for å fjære små korn som er igjen.
+    Etterpå dilates det slik at konturene av objektet blir litt kraftigere
+    :return:
+    """
+    # Her blures først bildet før det blir konvert til till HSV verdier
+    blur = cv2.GaussianBlur(frame, (7, 7), 0)
+    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+
+    # Morphing
+    mask = cv2.inRange(hsv, colorLower, colorUpper)
+    mask = cv2.erode(mask, None, erode)
+    return cv2.dilate(mask, None, dilate)
+
 def findHuMoments(image, frame):
+    """
+    Finds the hu-moments for the segmented images
+    :param image:
+    :param frame:
+    :return:
+    """
     global treerkast
     global firerkast
     global Score
+
     # Segementerer den tegnede kurven til ballen og finner så Hu-momentene
     mask_Hu = cv2.inRange(image, (0, 0, 254), (0, 0, 255))
 
@@ -255,24 +269,9 @@ def findHuMoments(image, frame):
     # Finner momentene til kurven. Så finner den Hu-momentene
     M = cv2.moments(mask_Hu)
     Hu = cv2.HuMoments(M).flatten()
-    # Man må visst ta log av Hu-momentene for best resultat
     Hu = -np.sign(Hu) * np.log10(np.abs(Hu))
 
-    # =============================================================================
-    #     Bruker knn på kastet og sammenlikner med treningssettet. Deretter klassifiserer den kastet
-    # =============================================================================
-    cnvToKNNformat = [[element for element in Hu]]
-    testSet = np.asarray(cnvToKNNformat, dtype=np.float32)
 
-    ret, Class, neighbours, dist = knn.findNearest(testSet, 15)
-    print(Class)
-    if Class == [[0.]]:
-        treerkast += 1
-    if Class == [[1.]]:
-        firerkast += 1
-
-    Feilbudsjett = (firerkast / (firerkast + treerkast)) * 100
-    Score = 'Score: ' + str(int(Feilbudsjett)) + '%'
 
     # =============================================================================
     #     #Laster inn treningssett og bruker regex til å hente ut ren data innenfor [---]
@@ -283,17 +282,45 @@ def findHuMoments(image, frame):
     #    storeHu.append(Hu)
     #    for item in storeHu:
     #        thefile.write("%s\n" % item)
-    return Class
+    return classify_with_knn(Hu)
 
 
-# =============================================================================
-# Plotter banen til kastet ett er det er nådd topppunktet
-# =============================================================================
-def drawTail(ptsTot, image, numOfObjects, counter):
+def classify_with_knn(Hu):
+    """
+    Use KNN to classify each throw
+    :param Hu:
+    :return:
+    """
+    global treerkast
+    global firerkast
+    global Score
+    # Bruker knn på kastet og sammenlikner med treningssettet. Deretter klassifiserer den kastet
+    cnvToKNNformat = [[element for element in Hu]]
+    testSet = np.asarray(cnvToKNNformat, dtype=np.float32)
+
+    ret, classification, neighbours, dist = knn.findNearest(testSet, 15)
+
+    if classification == [[0.]]:
+        treerkast += 1
+    if classification == [[1.]]:
+        firerkast += 1
+
+    Feilbudsjett = (firerkast / (firerkast + treerkast)) * 100
+    Score = 'Score: ' + str(int(Feilbudsjett)) + '%'
+
+    return classification
+
+def drawTail(ptsTot, image):
+    """
+    Plotter banen til kastet ett er det er nådd topppunktet
+    :param ptsTot:
+    :param image:
+    :return:
+    """
     s = copy.deepcopy(image)
     for j in range(1, len(ptsTot)):
 
-        # Hvis sdet ikke har er noe objekt så ikke tegn noe
+        # Hvis det ikke er noe objekt så ikke tegn noe
         if ptsTot[j - 1] == (0, 0) or ptsTot[j] == (0, 0):
             continue
         thickness = 5
@@ -303,12 +330,20 @@ def drawTail(ptsTot, image, numOfObjects, counter):
     return s
 
 
-# =============================================================================
-# CollectData() bergener topppunkt og bunnpunkt for kastene og hastigheten (endring fra frame til frame)
-# Den har en logikk som vet når ett kast er gjort, og beregner så Humomentene fra plottet til kastet.
-# =============================================================================
-def collectData(ptsTot, counter, frame, numOfObjects, radius, checkYdir, throwCounter):
-    global Class
+def collectData(ptsTot, counter, frame, numOfObjects, radius, checkYdir):
+    """
+    CollectData() bergener topppunkt og bunnpunkt for kastene og hastigheten (endring fra frame til frame)
+    Den har en logikk som vet når ett kast er gjort, og beregner så Humomentene fra plottet til kastet.
+    :param ptsTot:
+    :param counter:
+    :param frame:
+    :param numOfObjects:
+    :param radius:
+    :param checkYdir:
+    :param throwCounter:
+    :return:
+    """
+    global classification
     yret = 0
 
     for i in range(0, numOfObjects):
@@ -364,8 +399,8 @@ def collectData(ptsTot, counter, frame, numOfObjects, radius, checkYdir, throwCo
 
                 # Får lagret begge to på en gang
                 if checkYdir[i][0] < 0 and checkYdir[i][1] > 0:
-                    s = drawTail(ptsTot[i], frame, numOfObjects, counter)
-                    Class = findHuMoments(s, frame)
+                    s = drawTail(ptsTot[i], frame)
+                    classification = findHuMoments(s, frame)
                     throwHeight = (minHeight[1] - maxHeight[1]) / radius
                     storeHeight.append(throwHeight)
 
@@ -376,10 +411,10 @@ def collectData(ptsTot, counter, frame, numOfObjects, radius, checkYdir, throwCo
         cv2.putText(frame, "Throws: {}".format(len(storeHeight)), (10, 330), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0),
                     1)
 
-        if Class == [[0.]]:
+        if classification == [[0.]]:
             cv2.putText(frame, 'Throw type: 3', (10, 355), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 1)
 
-        elif Class == [[1.]]:
+        elif classification == [[1.]]:
             cv2.putText(frame, 'Throw type: 4', (10, 355), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 1)
     if Score != 0:
         cv2.putText(frame, Score, (10, 380), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 1)
